305bb4587b1dca4874cce1d79451bc1a0e2095ad
Andrey Viktorov <andrey.viktorov@gmail.com>
2021-08-04 05:51:18 +0700

    Fixed log import / export attributes

--- include/common/Log.h
+++ include/common/Log.h
@@ -1,11 +1,13 @@
 #pragma once
 
+#include <common/Common.h>
+
 class Log
 {
 public:
-	static void message(const char *format, ...);
-	static void warning(const char *format, ...);
-	static void error(const char *format, ...);
-	static void fatal(const char *format, ...);
+	static SCAPES_API void message(const char *format, ...);
+	static SCAPES_API void warning(const char *format, ...);
+	static SCAPES_API void error(const char *format, ...);
+	static SCAPES_API void fatal(const char *format, ...);
 };
 
