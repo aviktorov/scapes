c9458ca7c10f9e94dc7034a71bdf39f3cbe4762f
Andrey Viktorov <andrey.viktorov@gmail.com>
2021-11-29 02:02:18 +0700

    Included random utilities to math by default

--- include/scapes/foundation/math/Math.h
+++ include/scapes/foundation/math/Math.h
@@ -5,6 +5,7 @@
 #include <scapes/3rdparty/glm/glm.hpp>
 #include <scapes/3rdparty/glm/gtc/quaternion.hpp>
 #include <scapes/3rdparty/glm/gtc/matrix_transform.hpp>
+#include <scapes/3rdparty/glm/gtc/random.hpp>
 
 namespace scapes::foundation
 {
